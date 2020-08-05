1707f593cd30b94d767cd4a16b4ff668521730de
PYScourse <pyecourse@gmain.com>
2020-08-05 08:22:27 +0800

    标识符的笔记

--- src/day_02/test2.java
+++ src/day_02/test2.java
@@ -1,4 +1,13 @@
 package day_02;
 
 public class test2 {
+    public static void main(String[] args) {
+        //标识符指的是，凡是能自己命名的单词
+        //main属于标识符，但是不能修改，java开发者固定好的
+        //标识符只能以 数字、字母、下划线_ $开头
+        //不能以数字开头
+        //标识符中不能有空格
+        //关键字不能做标识符
+        //错误原因总结
+    }
 }
